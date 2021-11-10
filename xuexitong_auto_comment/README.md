6e031e1f6536a15b48e3beb80ba8bf31d2a3db7a
Ki-Seki <Ki_Seki@outlook.com>
2021-11-10 13:19:44 +0800

    xuexitong_auto_comment update

---
+++ xuexitong_auto_comment/README.md
@@ -0,0 +1,17 @@
+# 学习通自动评论
+
+## 介绍
+
+学习通网课的评分系统中，有一个叫做参与讨论。参与讨论可以通过**自己发布话题**或者**在别人已经发布的话题中进行评论**来实现。本程序原来是通过后者来实现的，但由于规则改变目前采用第一种方法实现，即模拟发布话题
+
+## 使用方法
+
+1. 保证电脑装有最新的 Edge 浏览器
+2. `pip install selenium`
+3. 下载最新的 [Edge Driver](https://developer.microsoft.com/en-us/microsoft-edge/tools/webdriver/)，并将可执行文件放在本文件夹中
+4. 在 configuration.py 中进行配置
+5. 启动 main.py
+
+## 注意
+
+* 全屏显示后，可以最小化后台运行
--- xuexitong_auto_comment/main.py
+++ xuexitong_auto_comment/main.py
@@ -1,36 +1,60 @@
 """
-自动刷学习通课程的评论，
+自动刷学习通课程的评论，仅支持旧版的学习通
+学习通的讨论页
 相关配置见 configuration.py
 """
 
-from selenium import webdriver
 import random
 import time
 
-import configuration
+from selenium import webdriver
+from selenium.common.exceptions import ElementNotInteractableException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-for url in configuration.reply_urls:
-    driver = webdriver.Edge("MicrosoftWebDriver.exe")
+import configuration as conf
+
+
+# 驱动实例化
+driver = webdriver.Edge("msedgedriver.exe")
+driver.maximize_window()  #浏览器最大化
+
+# 登录
+driver.get(conf.login_url)
+elem_phone = driver.find_element_by_css_selector(".ipt-tel")
+elem_password = driver.find_element_by_css_selector(".ipt-pwd")
+elem_login = driver.find_element_by_css_selector("#phoneLoginBtn")
+elem_phone.send_keys(conf.phone)
+elem_password.send_keys(conf.password)
+elem_login.click()
+
+# 检查是否登录上
+WebDriverWait(driver, 30).until(
+    EC.presence_of_element_located((By.ID, 'space_nickname')))
+
+for url in conf.discuss_urls:
     driver.get(url)
-
-    # login
-    elem_phone = driver.find_element_by_css_selector(".ipt-tel")
-    elem_password = driver.find_element_by_css_selector(".ipt-pwd")
-    elem_login = driver.find_element_by_css_selector("#loginBtn")
-    elem_phone.send_keys(configuration.phone)
-    elem_password.send_keys(configuration.password)
-    elem_login.click()
-    time.sleep(25)
-
-    # auto comment
-    elem_input = driver.find_element_by_css_selector(".replyEdit textarea")
-    elem_submit = driver.find_element_by_css_selector(".addReply")
-    for i in range(configuration.count):
-        elem_input.clear()
+    for index in range(conf.count):
+        # 添加话题
         time.sleep(random.random() * 3)
-        elem_input.send_keys(configuration.reply_content)
-        time.sleep(random.random() * 3)
-        elem_submit.click()
-        time.sleep(random.random() * 3)
+        add_topic = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'c_title')))
+        add_topic.send_keys(conf.reply_content[random.randint(0, len(conf.reply_content)-1)])
 
-    driver.close()
+        # 提交
+        time.sleep(random.random() * 3)
+        submit = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'a.qdBtn')))
+        time.sleep(random.random() * 3)
+        try:
+            submit.click()
+        except ElementNotInteractableException:
+            submit.click()  # 有时候因为没有加载完毕可能需要再点击一次
+        
+        # 跳过对话框
+        time.sleep(random.random() * 3)
+        alert = driver.switch_to.alert
+        alert.dismiss()
+
+        print(f'已发送 {index + 1} 条')
+
+driver.close()
