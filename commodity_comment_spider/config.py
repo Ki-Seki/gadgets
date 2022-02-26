6604235d5845f885eb241880d27ebaf7d2e61b19
Ki-Seki <Ki_Seki@outlook.com>
2022-02-26 16:00:32 +0800

    Finish Jingdong Spider

--- commodity_comment_spider/config.py
+++ commodity_comment_spider/config.py
@@ -1,17 +1,19 @@
-# Links of commodities to be parsed.
-commodity_links = [
-    'https://item.jd.com/100014415029.html?cu=true&utm_source=jd.idey.cn&utm_medium=tuiguang&utm_campaign=t_2024175271_&utm_term=e315934ac4f94f5ebdb3a906fa97b46b#comment',
-    'https://item.jd.com/29080892199.html?cu=true&utm_source=jd.idey.cn&utm_medium=tuiguang&utm_campaign=t_2024175271_&utm_term=f7e08f583d3245c28ed00b3f7aa42b20#none '
-]
+# Links of commodities to be scraped.
+commodity_links = {
+    '魅族 18s': 'https://item.jd.com/100014415029.html?cu=true&utm_source=jd.idey.cn&utm_medium=tuiguang&utm_campaign=t_2024175271_&utm_term=e315934ac4f94f5ebdb3a906fa97b46b#comment',
+    '燮乐 1克拉莫桑钻吊坠': 'https://item.jd.com/29080892199.html?cu=true&utm_source=jd.idey.cn&utm_medium=tuiguang&utm_campaign=t_2024175271_&utm_term=f7e08f583d3245c28ed00b3f7aa42b20#none',
+    '欧派整体橱柜': 'https://item.jd.com/43087414865.html',
+    '欧米茄(OMEGA)瑞士手表': 'https://item.jd.com/100001079146.html?cu=true&utm_source=jd.idey.cn&utm_medium=tuiguang&utm_campaign=t_2024175271_&utm_term=791fa84a9b2b401db855c15b3c994526',
+    '联想ThinkBook 14p': 'https://item.jd.com/100027378160.html?cu=true&utm_source=jd.idey.cn&utm_medium=tuiguang&utm_campaign=t_2024175271_&utm_term=45dc5de88e2a48c69286b9a948c41e9b',
+}
 
 # Maximum comment count you want to get from each commodity.
 # Please set to a multiple of 10.
 # Set to -1 to get all comments.
-max_comments = 30
+max_comments = 100
 
 # Name of the Excel file that stores the data.
 filename = 'data.xlsx'
 
-# User Agent
-user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 ' \
-             'Safari/537.36 Edg/98.0.1108.56 '
+# Interval between requests.
+interval = 3
